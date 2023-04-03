a93fb20ccc49e664048b3a84c3b9615bd473b5c9
Tim Sundell <tim920129@hotmail.com>
2023-04-04 00:12:40 +0200

    :construction: fixed log in router, checks if user exist

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -25,22 +25,53 @@ router.get('/', function(req, res, next) {
   })
 });
 
+// SKAPA USER
+
 router.post('/add', function(req, res, next) {
   const user = req.body;
   let userId = uuidv4();
   let encryptPassword = req.body.password;
   encryptPassword = CryptoJS.AES.encrypt(user.password, "salt key").toString();
-
+  
+  const checkExists = `SELECT * FROM users WHERE userEmail = '${user.email}' OR userName = '${user.name}'`;
   const sql = `INSERT INTO users (id, userName, userEmail, userPassword) VALUES ('${userId}','${user.name}', '${user.email}', '${encryptPassword}')`;
 
-  req.app.locals.con.query(sql, function(err, result){
-    if (result) {
-      res.status(201).json(result);
-    } else if (err) {
-      console.error(err);
-      res.status(500).json({ msg: err });
+  req.app.locals.con.query(checkExists, function(err, result){
+    if (result && result.length > 0) {
+      res.status(409).json({ msg: 'Email or name already exists.' });
+    } else {
+      req.app.locals.con.query(sql, function(err, result){
+        if (result) {
+          res.status(201).json(result);
+        } else if (err) {
+          console.error(err);
+          res.status(500).json({ msg: err });
+        }
+      });
     }
   });
 });
 
+// LOGGA IN USER 
+router.post('/login', function (req, res, next) {
+  const user = req.body;
+  const dbPassword = req.body.password;
+
+  const sql = `SELECT * FROM users WHERE userEmail = '${user.email}'`;
+  console.log(user.password);
+    req.app.locals.con.query(sql, function(err, result){
+      if (result) {  
+        let decryptPassword = CryptoJS.AES.decrypt(result[0].userPassword, "salt key").toString(CryptoJS.enc.Utf8);
+        if (decryptPassword === dbPassword){
+          res.status(200).json(result);
+        } else {
+          res.status(401).json({ msg: 'Incorrect email or password' });
+        }
+      } else {
+        console.error(err);
+        res.status(500).json({ msg: err });
+      }
+    });
+ 
+});
 module.exports = router;
